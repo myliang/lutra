eae1a7b08c6c74ce297c5513c993669ff2766944
myliang <liangyuliang0335@126.com>
2019-06-28 23:06:31 +0800

    web-components -> html, render

--- src/web-components/index.js
+++ src/web-components/index.js
@@ -1,9 +1,12 @@
 import Template from './template';
 import NodeExpr from './node-expr';
+import TemplateExpr from './template-expr';
+
+// const stringsCache = new Map();
+NodeExpr.getTemplateExpr = strings => new TemplateExpr(strings);
 
 // cache
 const containerCache = new Map();
-
 // gobal methods
 export function html(strings, ...values) {
   return new Template(strings, ...values);
--- src/web-components/node-expr.js
+++ src/web-components/node-expr.js
@@ -1,8 +1,7 @@
 /* global document */
+/* global Node */
 import Expr from './expr';
 import Template from './template';
-import TemplateCache from './template-cache';
-import TemplateExprs from './template-exprs';
 
 function cc() {
   return document.createComment('');
@@ -11,90 +10,6 @@ function cc() {
 function insert(node) {
   this.e.parentNode.insertBefore(node, this.e);
 }
-
-function updateText(value) {
-  const { s, e } = this;
-  const node = e.previousSibling;
-  if (node !== s) {
-    node.textContent = value;
-  } else {
-    insert.call(this, document.createTextNode(value));
-  }
-  this.v = value;
-}
-
-function updateNode(value) {
-  if (this.v !== value) {
-    insert.call(this, value);
-    this.v = value;
-  }
-}
-
-// value: Template
-function updateTemplate(value) {
-  // console.log('value:', value, this.v);
-  const tcache = TemplateCache.get(value);
-  // console.log('tcache:', tcache, this.v);
-  if (this.v && this.v.tcache === tcache) {
-    this.v.update(value.values);
-  } else {
-    const templateExprs = new TemplateExprs(tcache);
-    const content = templateExprs.clone();
-    templateExprs.update(value.values);
-    this.v = templateExprs;
-    // console.log('templateExprs:', templateExprs);
-    insert.call(this, content);
-  }
-}
-
-function updateArray(value) {
-  // console.log('this:', this);
-  this.v = this.v || [];
-  // console.log('v:', this.v);
-  // const root = NodeExpr.insertAfter(this.e);
-
-  let lastIndex = 0;
-  let expr;
-  // console.log('value:', value);
-  value.forEach((item, index) => {
-    expr = this.v[index];
-    // console.log('index:', expr,  index, item);
-    if (expr === undefined) {
-      if (index === 0) {
-        expr = NodeExpr.insertAfter(this.e);
-      } else {
-        // console.log('v.last:', this.v[this.v.length - 1]);
-        expr = NodeExpr.insertAfter(this.v[this.v.length - 1].e);
-      }
-      this.v.push(expr);
-    }
-    expr.update(item);
-    lastIndex = index;
-  });
-  lastIndex += 1;
-  // console.log('v:', this.v, lastIndex);
-  if (lastIndex < this.v.length) {
-    const { s } = this.v[lastIndex];
-    this.v.length = lastIndex;
-    // console.log('expr:', expr);
-    let node = s.nextSibling;
-    while (node) {
-      const n = node.nextSibling;
-      s.parentNode.removeChild(node);
-      node = n;
-    }
-  }
-}
-
-function updatePromise(value) {
-  this.v = value;
-  value.then((v) => {
-    if (this.v === value) {
-      this.update(v);
-    }
-  });
-}
-
 
 export default class NodeExpr extends Expr {
   constructor(snode, enode) {
@@ -109,17 +24,95 @@ export default class NodeExpr extends Expr {
     // console.log('vtype:', vtype, value instanceof Template);
     if (vtype !== 'function' && vtype !== 'object') {
       if (value !== v) {
-        updateText.call(this, value);
+        this.updateText(value);
       }
     } else if (value instanceof Template) {
-      updateTemplate.call(this, value);
+      this.updateTemplate(value);
     } else if (value instanceof Node) {
-      updateNode.call(this, value);
+      this.updateNode(value);
     } else if (Array.isArray(value)) {
-      updateArray.call(this, value);
+      this.updateArray(value);
     } else if (value.then !== undefined) {
-      updatePromise.call(this, value);
+      this.updatePromise(value);
     }
+  }
+
+  updateText(value) {
+    const { s, e } = this;
+    const node = e.previousSibling;
+    if (node !== s) {
+      node.textContent = value;
+    } else {
+      insert.call(this, document.createTextNode(value));
+    }
+    this.v = value;
+  }
+
+  updateNode(value) {
+    if (this.v !== value) {
+      insert.call(this, value);
+      this.v = value;
+    }
+  }
+
+  // value: Template
+  updateTemplate({ strings, values }) {
+    if (this.v === undefined) {
+      const t = NodeExpr.getTemplateExpr(strings);
+      const content = t.clone();
+      // console.log('templateExprs:', templateExprs);
+      insert.call(this, content);
+      this.v = t;
+    }
+    this.v.update(values);
+  }
+
+  updateArray(value) {
+    // console.log('this:', this);
+    this.v = this.v || [];
+    // console.log('v:', this.v);
+    // const root = NodeExpr.insertAfter(this.e);
+
+    let lastIndex = 0;
+    let expr;
+    // console.log('value:', value);
+    value.forEach((item, index) => {
+      expr = this.v[index];
+      // console.log('index:', expr,  index, item);
+      if (expr === undefined) {
+        if (index === 0) {
+          expr = NodeExpr.insertAfter(this.e);
+        } else {
+          // console.log('v.last:', this.v[this.v.length - 1]);
+          expr = NodeExpr.insertAfter(this.v[this.v.length - 1].e);
+        }
+        this.v.push(expr);
+      }
+      expr.update(item);
+      lastIndex = index;
+    });
+    lastIndex += 1;
+    // console.log('v:', this.v, lastIndex);
+    if (lastIndex < this.v.length) {
+      const { s } = this.v[lastIndex];
+      this.v.length = lastIndex;
+      // console.log('expr:', expr);
+      let node = s.nextSibling;
+      while (node) {
+        const n = node.nextSibling;
+        s.parentNode.removeChild(node);
+        node = n;
+      }
+    }
+  }
+
+  updatePromise(value) {
+    this.v = value;
+    value.then((v) => {
+      if (this.v === value) {
+        this.update(v);
+      }
+    });
   }
 
   static root(container) {
