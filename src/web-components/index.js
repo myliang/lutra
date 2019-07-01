179c3790b574ba31aa42658c36fc57a4cf44dbd0
myliang <liangyuliang0335@126.com>
2019-07-01 15:40:53 +0800

    color-palette rewrite by web components

--- src/web-components/index.js
+++ src/web-components/index.js
@@ -20,3 +20,43 @@ export function render(template, container) {
   }
   root.update(template);
 }
+
+// decorators
+// customElements.define('xfd-color-palette', ColorPalette);
+export function component(name, style) {
+  return (target) => {
+    customElements.define(name, target);
+    target.style = style;
+  };
+}
+
+// export function css(strings, ...vs) {
+  // const cssText = vs.reduce((prev, v, i) => `${prev}${v}${strings[i + 1]}`, strings[0]);
+  // return new CSSResult(cssText, 
+// }
+
+export class BaseElement extends HTMLElement{
+  constructor() {
+    super();
+    this.change = () => {};
+    // console.log('constructor:', this.constructor.style);
+    const shadow = this.attachShadow({ mode: 'open' });
+    shadow.adoptedStyleSheets = this.constructor.getStyleSheets();
+    render(this.render(), shadow);
+  }
+
+  render() {}
+
+  static getStyleSheets() {
+    const { style } = this;
+    if (style) {
+      // console.log('style:', style);
+      const cssStyle = new CSSStyleSheet();
+      cssStyle.replaceSync(style);
+      console.log('css:style', cssStyle);
+      return [cssStyle];
+    } else {
+      return [];
+    }
+  }
+}
