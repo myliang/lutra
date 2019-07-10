c297ddeddeaa1ad27e9c9a620d726dbbaba060c5
myliang <liangyuliang0335@126.com>
2019-07-10 09:13:40 +0800

    top - 1, left - 1 for editor and selector

--- src/web-component/editor.js
+++ src/web-component/editor.js
@@ -1,11 +1,19 @@
 import {
-  html, BaseElement, component,
+  html, BaseElement, component, element,
 } from './core';
 
 function loop() {}
 
 function inputHandler({ target }) {
   this.change(target.value, 'changed');
+}
+
+function setSelectionRange(position) {
+  setTimeout(() => {
+    const el = element.call(this, 'textarea');
+    el.focus();
+    el.setSelectionRange(position, position);
+  }, 0);
 }
 
 export default @component('xfd-editor')
@@ -15,12 +23,14 @@ class Editor extends BaseElement {
     const {
       left, top, width, height,
     } = offset;
-    this.setOffset({ left, top });
+    this.setOffset({ left: left - 1, top: top - 1 });
+    setSelectionRange.call(this, content.length);
     return html`
       <textarea
         style="${{ width: width - 9, height: height - 3 }}"
         @input="${inputHandler.bind(this)}"
         .value="${content}"
+        @change.stop="${loop}"
         @mousedown.stop="${loop}"
         @mousemove.stop="${loop}"></textarea>
       <div class="textline"></div>
--- src/web-component/selector.js
+++ src/web-component/selector.js
@@ -5,14 +5,19 @@ import {
 export default @component('xfd-selector')
 class Selector extends BaseElement {
   render() {
-    const { offset } = this.$props;
-    let { width, height } = offset;
+    let {
+      left, top, width, height,
+    } = this.$props.offset;
     // console.log('selector.offset:', offset);
     // const { visible } = this.$state;
     // this.style.display = visible ? 'block' : 'none';
+    left -= 1;
+    top -= 1;
     width -= 3;
     height -= 3;
-    this.setOffset(offset);
+    this.setOffset({
+      left, top, width, height,
+    });
     return html`
     <div class="area" style="${{ width, height }}"></div>
     <div class="corner"></div>
