260d212ca39fd55937070649446bab21c91c2567
myliang <liangyuliang0335@126.com>
2019-07-14 23:26:14 +0800

    form-type[checkbox, radios...] support for drag

--- src/web-component/form-palette.js
+++ src/web-component/form-palette.js
@@ -1,11 +1,34 @@
+/* global document */
 import { html, BaseElement, component } from './core';
 import { t } from '../locale/locale';
+
+function dragstart(type, { dataTransfer, target }) {
+  const crt = document.createElement('div');
+  crt.className = 'lutra-drag';
+  crt.innerHTML = target.innerHTML;
+  target.dragEl = crt;
+  document.body.appendChild(crt);
+  dataTransfer.setDragImage(crt, 20, 10);
+  dataTransfer.dropEffect = 'move';
+  dataTransfer.setData('type', type);
+  return true;
+}
+
+function dragend({ dataTransfer, target }) {
+  document.body.removeChild(target.dragEl);
+  dataTransfer.clearData('type');
+  return false;
+}
 
 function buildItems() {
   const { items } = this.$props;
   return Object.entries(items).map(([k, v]) => {
     const name = v.name || t(`form.${k}`);
-    return html`<li>${name}</li>`;
+    return html`
+    <li draggable="true"
+      @dragend="${dragend.bind(this)}"
+      @dragstart="${dragstart.bind(this, k)}">${name}</li>
+    `;
   });
 }
 
